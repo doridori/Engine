55746efc9e4dd3b71f6d37a7d637d03abef35376
Dorian Cussen <dori@kodroid.com>
2015-11-26 17:38:33 +0000

    put in correct package structure!

--- src/main/java/com/kodroid/engine/FsmEngine.java
+++ src/main/java/com/kodroid/engine/FsmEngine.java
@@ -1,3 +1,5 @@
+package com.kodroid.engine;
+
 import java.util.HashMap;
 import java.util.Map;
 
--- src/test/java/com/kodroid/engine/FsmEngineTest.java
+++ src/test/java/com/kodroid/engine/FsmEngineTest.java
@@ -1,3 +1,5 @@
+package com.kodroid.engine;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
